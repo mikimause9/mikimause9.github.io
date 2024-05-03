a9cd8b5f036984be575cb7be2bad9be90a2c93e1
mikimause9 <mikolajek_zdan@icloud.com>
2024-05-03 22:05:53 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById("passwordForm").addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevent form submission
+    var password = document.getElementById("passwordInput").value;
+    if (password === "idkwtwt") {
+        window.location.href = "letter.html?haslo=" + encodeURIComponent(password);
+    } else {
+        window.location.href = "letter.html";
+    }
+});

5d7aefb8319ab93292f465aada572ead466261fe
mikimause9 <mikolajek_zdan@icloud.com>
2024-05-04 15:00:10 +0000

    Initial commit

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
+var urlParams = new URLSearchParams(window.location.search);
+var infoParam = urlParams.get('info');
+
+if (infoParam === "urodzinki19") {
+    window.location.href = "zaproszenie_urodzinki_19.html";
+}
+
 document.getElementById("passwordForm").addEventListener("submit", function (event) {
     event.preventDefault(); // Prevent form submission
     var password = document.getElementById("passwordInput").value;
-    if (password === "urodzinki19") {
-        window.location.href = "letter.html?haslo=" + encodeURIComponent(password);
-    } else {
-        window.location.href = "letter.html";
-    }
+    //        if (password === "urodzinki19") {
+    //            window.location.href = "zaproszenie_urodzinki_19.html";
+    //        } else {
+    window.location.href = "default.html?haslo=" + encodeURIComponent(password);
+    //        }
+
+
 });
+
